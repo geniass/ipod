535563ac3bf2849d668c9c5494ce69006d73ea34
Ari Croock <aricroock@gmail.com>
2019-03-06 22:09:58 +0200

    WIP: dbus functions for accessory button controls

--- lingo-extremote/handler.go
+++ lingo-extremote/handler.go
@@ -1,7 +1,14 @@
 package extremote
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/oandrew/ipod"
+
+	"github.com/godbus/dbus"
+
+	"log"
 )
 
 type DeviceExtRemote interface {
@@ -16,7 +23,57 @@ func ackSuccess(req *ipod.Command) *ACK {
 // 	return ACKPending{Status: ACKStatusPending, CmdID: uint8(req.ID.CmdID()), MaxWait: maxWait}
 // }
 
+func getTrackMetadata(connection *dbus.Conn, playerPath dbus.ObjectPath) (map[string]interface{}, error) {
+	if playerPath == "" {
+		return nil, errors.New("playerPath is empty")
+	}
+
+	player := connection.Object("org.bluez", playerPath)
+	track, err := player.GetProperty("org.bluez.MediaPlayer1.Track")
+	if err != nil {
+		return nil, errors.New("could not get track from dbus: " + err.Error())
+	}
+
+	t, ok := track.Value().(map[string]dbus.Variant)
+	if ok {
+		return map[string]interface{}{"title": t["Title"].String(), "artist": t["Artist"].String()}, nil
+	}
+	return nil, errors.New("Could not coerce track to map")
+}
+
+func getPlayerStatus(connection *dbus.Conn, playerPath dbus.ObjectPath) (string, error) {
+	if playerPath == "" {
+		return "", errors.New("playerPath is empty")
+	}
+
+	player := connection.Object("org.bluez", playerPath)
+	status, err := player.GetProperty("org.bluez.MediaPlayer1.Status")
+	if err != nil {
+		return "", errors.New("could not get player status from dbus: " + err.Error())
+	}
+
+	return status.Value().(string), nil
+}
+
 func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemote) error {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		panic(err)
+	}
+	var objects map[dbus.ObjectPath]map[string]map[string]dbus.Variant
+	conn.Object("org.bluez", "/").Call("org.freedesktop.DBus.ObjectManager.GetManagedObjects", 0).Store(&objects)
+
+	var playerPath dbus.ObjectPath
+	for path, obj := range objects {
+		for inter := range obj {
+			if inter == "org.bluez.MediaPlayer1" {
+				playerPath = path
+				break
+			}
+		}
+	}
+	log.Printf("playerPath: %v", playerPath)
+
 	//log.Printf("Req: %#v", req)
 	switch msg := req.Payload.(type) {
 
@@ -113,6 +170,89 @@ func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemo
 	case *PlayCurrentSelection:
 		ipod.Respond(req, tr, ackSuccess(req))
 	case *PlayControl:
+		payload := req.Payload.(*PlayControl)
+		switch payload.Cmd {
+		case PlayControlToggle:
+			status, err := getPlayerStatus(conn, playerPath)
+			log.Printf("Play status from dbus: " + status)
+			if err != nil {
+				log.Printf("ERROR: getting play status from dbus: %s", err.Error())
+				if playerPath != "" {
+					err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+					if err != nil {
+						log.Printf("ERROR: calling Play: %v", err)
+					}
+				}
+			} else {
+				switch status {
+				case "playing":
+					if playerPath != "" {
+						err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Pause", 0).Store()
+						if err != nil {
+							log.Printf("ERROR: calling Pause: %v", err)
+						}
+					}
+				case "stopped":
+					if playerPath != "" {
+						err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+						if err != nil {
+							log.Printf("ERROR: calling Pause: %v", err)
+						}
+					}
+				case "paused":
+					if playerPath != "" {
+						err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+						if err != nil {
+							log.Printf("ERROR: calling Pause: %v", err)
+						}
+					}
+				default:
+					if playerPath != "" {
+						err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+						if err != nil {
+							log.Printf("ERROR: calling Play: %v", err)
+						}
+					}
+				}
+			}
+		case PlayControlPlay:
+			log.Print("Play")
+			if playerPath != "" {
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Play", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Play: %v", err)
+				}
+			}
+			return nil
+		case PlayControlPause:
+			log.Print("Pause")
+			if playerPath != "" {
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Pause", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Pause: %v", err)
+				}
+			}
+			return nil
+		case PlayControlNextTrack, PlayControlNextChapter, PlayControlNext:
+			log.Print("Next")
+			if playerPath != "" {
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Next", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Next: %v", err)
+				}
+			}
+			return nil
+		case PlayControlPrevTrack, PlayControlPrevChapter, PlayControlPrev:
+			log.Print("Prev")
+			if playerPath != "" {
+				err := conn.Object("org.bluez", playerPath).Call("org.bluez.MediaPlayer1.Previous", 0).Store()
+				if err != nil {
+					log.Printf("ERROR: calling Previous: %v", err)
+				}
+			}
+			ipod.Respond(req, tr, &ACK{Status: ACKStatus(PlayControlPrevTrack), CmdID: req.ID.CmdID()})
+			return nil
+		}
 		ipod.Respond(req, tr, ackSuccess(req))
 	case *GetTrackArtworkTimes:
 		ipod.Respond(req, tr, &RetTrackArtworkTimes{})
